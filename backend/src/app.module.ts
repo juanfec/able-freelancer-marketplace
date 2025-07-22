0411068cd7d07ee4a2335342b1c8e91cbb3f158e
Juan Felipe Rueda <juan.felipe.rueda@gmail.com>
2025-07-21 20:35:12 -0500

    adding db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST || 'localhost',
+      port: process.env.PGPORT ? parseInt(process.env.PGPORT, 10) : 5432,
+      username: process.env.PGUSER || 'postgres',
+      password: process.env.PGPASSWORD || 'postgres',
+      database: process.env.PGDATABASE || 'app_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
