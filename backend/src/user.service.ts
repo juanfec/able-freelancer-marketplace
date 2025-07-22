3a590526d0d53175b730f861daa25a5235cfb6c2
Juan Felipe Rueda <juan.felipe.rueda@gmail.com>
2025-07-21 21:17:26 -0500

    Update user.service.ts

--- backend/src/user.service.ts
+++ backend/src/user.service.ts
@@ -1,33 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
 
   create(data: Partial<User>) {
+    this.logger.log(`Creating user with email: ${data.email}`);
     const user = this.userRepository.create(data);
     return this.userRepository.save(user);
   }
 
   findAll() {
+    this.logger.log('Fetching all users');
     return this.userRepository.find();
   }
 
   findOne(id: number) {
+    this.logger.log(`Fetching user with id: ${id}`);
     return this.userRepository.findOneBy({ id });
   }
 
   update(id: number, data: Partial<User>) {
+    this.logger.log(`Updating user with id: ${id}`);
     return this.userRepository.update(id, data);
   }
 
   remove(id: number) {
+    this.logger.log(`Deleting user with id: ${id}`);
     return this.userRepository.delete(id);
   }
 }
